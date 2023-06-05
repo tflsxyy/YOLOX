e753d29e843a973f79f0d82cf2c30e0b2fc622c5
tflsxyy <16307130030@fudan.edu.cn>
2023-06-05 07:00:00 -0700

    update dacsdc dataset

--- dacsdc/dacsdc.py
+++ dacsdc/dacsdc.py
@@ -8,6 +8,6 @@ import cv2
 import numpy as np
 
 from ..dataloading import get_yolox_datadir
-from .datasets_wrapper import CacheDataset, cache_read_img
+from yolox.data.datasets.datasets_wrapper import CacheDataset, cache_read_img
 from .dacsdc_classes import DACSDC_CLASSES
 

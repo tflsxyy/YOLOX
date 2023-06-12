f6502125de10a0dfa24e362af32cfaa4b44b5153
tflsxyy <16307130030@fudan.edu.cn>
2023-06-11 19:04:19 -0700

    update README.md

---
+++ dacsdc/gdrive.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+fileid="1ceQ5y_rCReSZ26HzzCf2muDNbovjyl5k"
+filename="train.zip"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
